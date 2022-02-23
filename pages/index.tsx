55a0e2719b4a4cc41197bbbea60ef17392421ab0
wujovic <veljkovujovic185@gmail.com>
2022-02-23 17:57:08 +0100

    implementing SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,34 @@ import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 
-export default function Home() {
+export default function Home(props: any) {
   return (
     <div>
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Pull some data from a server - API endpoints */}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
