33618ccac890a3efa778dc3ad82132ed0dc8ccbd
wujovic <veljkovujovic185@gmail.com>
2022-02-23 19:31:20 +0100

    large card section done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
@@ -41,6 +42,12 @@ export default function Home(props: any) {
             ))}
           </div>
         </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Whislists curated by airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   );
