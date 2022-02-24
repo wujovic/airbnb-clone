0638b5b2c11e47d3db4dee3bc53fd9573acc6416
wujovic <veljkovujovic185@gmail.com>
2022-02-24 17:30:22 +0100

    search page done, cards done

--- pages/search.tsx
+++ pages/search.tsx
@@ -3,12 +3,16 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useRouter } from "next/router";
 import { format } from "date-fns";
+import InfoCard from "../components/InfoCard";
 
-function Search() {
+function Search(props: any) {
   const router = useRouter();
+  const searchResults = props.searchResults;
 
   const { location, startDate, endDate, numberOfGuests } = router.query;
+  //@ts-ignore
   const formattedStartDate = format(new Date(startDate), "dd MMMM YYY");
+  //@ts-ignore
   const formattedEndDate = format(new Date(endDate), "dd MMMM YYY");
 
   return (
@@ -22,15 +26,32 @@ function Search() {
             300+ Stays - {formattedStartDate} - {formattedEndDate} - guests:{" "}
             {numberOfGuests}
           </p>
+
           <h1 className="text-3xl font-semibold mt-2 mb-6">
             Stays in {location}
           </h1>
+
           <div className="hidden md:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="button">Cancelation Flexibility</p>
             <p className="button">Type of Place</p>
             <p className="button">Price</p>
             <p className="button">Rooms and Beds</p>
             <p className="button">More Filters</p>
+          </div>
+
+          <div className="flex flex-col">
+            {searchResults?.map((item: any) => (
+              <InfoCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                title={item.title}
+                description={item.description}
+                star={item.star}
+                price={item.price}
+                total={item.total}
+              />
+            ))}
           </div>
         </section>
       </main>
@@ -40,3 +61,15 @@ function Search() {
 }
 
 export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch("https://links.papareact.com/isz").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
